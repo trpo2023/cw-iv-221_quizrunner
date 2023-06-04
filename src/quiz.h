71a7c98cb1fb68201b807885e145c96447064c0e
igorNIkansin <imim.god@yandex.ru>
2023-06-04 17:24:20 +0700

    add codestyle

--- src/quiz.h
+++ src/quiz.h
@@ -1,24 +1,22 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
-#include <locale.h>
+#include <time.h>
+#include <stdbool.h>
 // Задайте количество вопросов
 #define NUM_QUESTIONS 9
 
 // Структура для хранения информации о вопросах и ответах
-typedef struct
-{
-    char *question;
-    char *answer;
+typedef struct {
+    char* question;
+    char* answer;
 } question_t;
-void create_questions(question_t *questions);
+void create_questions(question_t* questions);
 
 bool check_answer(char* answer1, char* answer2);
 // Функция для перемешивания вопросов
-void shuffle_questions(question_t *questions);
-
+void shuffle_questions(question_t* questions);
 
 // Функция для проведения теста
-void run_test(question_t *questions);
-
+void run_test(question_t* questions);
