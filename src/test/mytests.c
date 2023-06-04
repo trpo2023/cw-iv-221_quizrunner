b094087f8ec984cf08fca296b09398f56134ebbe
igorNIkansin <imim.god@yandex.ru>
2023-06-04 16:47:22 +0700

    add mytests file

---
+++ src/test/mytests.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include <stdbool.h>
+#include"ctest.h"
+#include"quiz.h"
+
+
+CTEST(suite1,test1){
+    bool ch;
+    bool ch1;
+    bool exp=true;
+    bool exp1=false;
+    ch=check_answer("aaaaaa","aaaaaa");
+    ch1=check_answer("aaaaaa","bbbbb");
+    ASSERT_EQUAL(ch,exp);
+    ASSERT_EQUAL(ch1,exp1);
+}
