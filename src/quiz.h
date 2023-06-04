8ecf4552af4c7978cd655bf5a74abdb2887dcc1a
igorNIkansin <imim.god@yandex.ru>
2023-06-04 17:32:21 +0700

    add codestyle

--- src/quiz.h
+++ src/quiz.h
@@ -1,9 +1,9 @@
 #include <locale.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <stdbool.h>
 // Задайте количество вопросов
 #define NUM_QUESTIONS 9
 
