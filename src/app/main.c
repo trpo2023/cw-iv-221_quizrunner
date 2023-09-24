1948010d8f1fecb13d14bd1a10269fddc4ab0c60
igorNIkanshin <igorNIkanshin@users.noreply.github.com>
2023-09-24 18:47:01 +0700

    change codestyle

--- src/app/main.c
+++ src/app/main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "lib/quiz.h"
+#include <stdio.h>
 
 int main()
 {
@@ -7,9 +7,12 @@ int main()
 
     questions[0] = createQuestion("Какая столица Франции?", "Париж");
     questions[1] = createQuestion("Сколько планет в солнечной системе?", "8");
-    questions[2] = createQuestion("Какой год начала Второй мировой войны?", "1939");
-    questions[3] = createQuestion("Какой химический элемент имеет символ 'H'?", "Водород");
-    questions[4] = createQuestion("Какой самый высокий горный пик в мире?", "Эверест");
+    questions[2]
+            = createQuestion("Какой год начала Второй мировой войны?", "1939");
+    questions[3] = createQuestion(
+            "Какой химический элемент имеет символ 'H'?", "Водород");
+    questions[4] = createQuestion(
+            "Какой самый высокий горный пик в мире?", "Эверест");
 
     shuffleQuestions(questions, 5);
 
--- src/lib/quiz.c
+++ src/lib/quiz.c
@@ -1,10 +1,10 @@
+#include "quiz.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "quiz.h"
 
-struct Question createQuestion(const char *text, const char *answer)
+struct Question createQuestion(const char* text, const char* answer)
 {
     struct Question question;
     strcpy(question.text, text);
@@ -12,7 +12,7 @@ struct Question createQuestion(const char *text, const char *answer)
     return question;
 }
 
-int checkAnswer(const char *userAnswer, const char *correctAnswer)
+int checkAnswer(const char* userAnswer, const char* correctAnswer)
 {
     return strcmp(userAnswer, correctAnswer) == 0;
 }
@@ -20,8 +20,7 @@ int checkAnswer(const char *userAnswer, const char *correctAnswer)
 void shuffleQuestions(struct Question questions[], int numQuestions)
 {
     srand(time(NULL));
-    for (int i = numQuestions - 1; i > 0; i--)
-    {
+    for (int i = numQuestions - 1; i > 0; i--) {
         int j = rand() % (i + 1);
         struct Question temp = questions[i];
         questions[i] = questions[j];
@@ -35,25 +34,23 @@ int runQuiz(struct Question questions[], int numQuestions)
 
     printf("Добро пожаловать в викторину!\n");
 
-    for (int i = 0; i < numQuestions; i++)
-    {
+    for (int i = 0; i < numQuestions; i++) {
         printf("\nВопрос %d: %s\n", i + 1, questions[i].text);
 
         char userAnswer[256];
         printf("Ваш ответ: ");
         scanf("%s", userAnswer);
 
-        if (checkAnswer(userAnswer, questions[i].answer))
-        {
+        if (checkAnswer(userAnswer, questions[i].answer)) {
             printf("Правильно!\n");
             score++;
-        }
-        else
-        {
+        } else {
             printf("Неправильно. Правильный ответ: %s\n", questions[i].answer);
         }
     }
 
-    printf("\nИгра окончена! Вы набрали %d из %d баллов.\n", score, numQuestions);
+    printf("\nИгра окончена! Вы набрали %d из %d баллов.\n",
+           score,
+           numQuestions);
     return score;
 }
