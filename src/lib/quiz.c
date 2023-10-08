a5d897894b0f3bae5e7d89cf6594771e42925a75
Pr0st0ychell <Pr0st0ychell@users.noreply.github.com>
2023-10-08 18:06:47 +0700

    add to_lower

--- src/lib/quiz.c
+++ src/lib/quiz.c
@@ -3,12 +3,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
+
+void to_lower(char *c, char *b) {
+    if (*c == -48 && (*b >= -125 && *b <= -97)) {
+        *b = *b + ('а' - 'А');
+    }
+
+    if (*c == -48 && *b >= -96 && *b <= -81) {
+        *c = *c + 1;
+        *b = *b - ('а' - 'А');
+    }
+}
+
 
 struct Question createQuestion(const char* text, const char* answer)
 {
     struct Question question;
     strcpy(question.text, text);
-    strcpy(question.answer, answer);
+    strcpy(question.answer, answer); 
     return question;
 }
 
@@ -31,6 +44,7 @@ void shuffleQuestions(struct Question questions[], int numQuestions)
 int runQuiz(struct Question questions[], int numQuestions)
 {
     int score = 0;
+    
 
     printf("Добро пожаловать в викторину!\n");
 
@@ -40,12 +54,22 @@ int runQuiz(struct Question questions[], int numQuestions)
         char userAnswer[256];
         printf("Ваш ответ: ");
         scanf("%s", userAnswer);
+        
+        for(int y=0;userAnswer[y] != '\0'; y = y + 2){
+            to_lower(&userAnswer[y], &userAnswer[y+1]);
+
+        }
+
+        for(int y=0;questions[i].answer[y] != '\0'; y = y + 2){
+            to_lower(&questions[i].answer[y], &questions[i].answer[y+1]);
+            
+        }
 
         if (checkAnswer(userAnswer, questions[i].answer)) {
             printf("Правильно!\n");
             score++;
         } else {
-            printf("Неправильно. Правильный ответ: %s\n", questions[i].answer);
+            printf("Неправильно. Правильный ответ: '%s'\n", questions[i].answer);
         }
     }
 
