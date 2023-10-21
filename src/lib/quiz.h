bd072b5e63f43bba639a1ccf283400162018c9c9
Kostenko Danil <dkostenko5566@mail.ru>
2023-10-21 17:18:02 +0700

    tests

--- src/lib/quiz.h
+++ src/lib/quiz.h
@@ -8,10 +8,7 @@ struct Question {
 
 struct Question createQuestion(const char* text, const char* answer);
 int checkAnswer(const char* userAnswer, const char* correctAnswer);
-struct Question*
-shuffleQuestions(struct Question questions[], int numQuestions);
-int getQuestionCount(const char* file);
-struct Question* readQuestionsFromFile(const char* file);
+void shuffleQuestions(struct Question questions[], int numQuestions);
 int runQuiz(struct Question questions[], int numQuestions);
 
 #endif // QUIZ_H
