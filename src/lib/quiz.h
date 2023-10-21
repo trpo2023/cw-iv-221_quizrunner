fa2a70107960f619c642d4c8f814961cccfa0dcf
Kostenko Danil <dkostenko5566@mail.ru>
2023-10-21 17:46:57 +0700

    check codestyle

--- src/lib/quiz.h
+++ src/lib/quiz.h
@@ -8,7 +8,8 @@ struct Question {
 
 struct Question createQuestion(const char* text, const char* answer);
 int checkAnswer(const char* userAnswer, const char* correctAnswer);
-struct Question* shuffleQuestions(struct Question questions[], int numQuestions);
+struct Question*
+shuffleQuestions(struct Question questions[], int numQuestions);
 int runQuiz(struct Question questions[], int numQuestions);
 
 #endif // QUIZ_H
