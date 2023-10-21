5cbab5f488b498b7f4b9bdb2f8cc2d2a26898882
Kostenko Danil <dkostenko5566@mail.ru>
2023-10-21 18:28:26 +0700

    or not

--- src/lib/quiz.h
+++ src/lib/quiz.h
@@ -1,6 +1,8 @@
 #ifndef QUIZ_H
 #define QUIZ_H
 
+#define MAX_LINE_LENGTH 1000
+
 struct Question {
     char text[256];
     char answer[256];
@@ -10,5 +12,7 @@ struct Question createQuestion(const char* text, const char* answer);
 int checkAnswer(const char* userAnswer, const char* correctAnswer);
 void shuffleQuestions(struct Question questions[], int numQuestions);
 int runQuiz(struct Question questions[], int numQuestions);
+int getQuestionCount(const char* filePath); // Объявление функции в .h файле
+struct Question* readQuestionsFromFile(const char* filePath); // Объявление функции в .h файле
 
 #endif // QUIZ_H
